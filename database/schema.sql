03067983beac525ed32a3c4bea7e8e27fd4d4983
Shiwei Chen <shiweichenus@gmail.com>
2022-08-09 19:01:17 -0500

    Minor update schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,41 +1,39 @@
 DROP DATABASE IF EXISTS sdc_qna;
 CREATE DATABASE sdc_qna;
 
-USE sdc_qna;
+DROP SCHEMA IF EXISTS qna CASCADE;
+CREATE SCHEMA qna;
 
-DROP SCHEMA IF EXISTS questions;
+DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
-  question_id int NOT NULL,
+  question_id int NOT NULL PRIMARY KEY,
   product_id int,
 	question_body varchar(255),
 	question_date varchar(255),
 	asker_name varchar(255),
 	asker_email varchar(255),
-	question_reported boolean,
-	question_helpfulness int,
-  PRIMARY KEY (question_id)
+	question_reported boolean DEFAULT false,
+	question_helpfulness int DEFAULT 0
 );
 
-DROP SCHEMA IF EXISTS answers;
+DROP TABLE IF EXISTS answers;
 CREATE TABLE answers (
-  answer_id int NOT NULL,
+  answer_id int NOT NULL PRIMARY KEY,
   answer_body varchar(255),
   answer_date varchar(255),
 	answerer_name varchar(255),
   answerer_email varchar(255),
-	answer_reported boolean,
-	answer_helpfulness int,
+	answer_reported boolean DEFAULT false,
+	answer_helpfulness int DEFAULT 0,
 	question_id int,
-  PRIMARY KEY (answer_id),
   FOREIGN KEY (question_id) REFERENCES questions(question_id)
 );
 
-DROP SCHEMA IF EXISTS photos;
+DROP TABLE IF EXISTS photos;
 CREATE TABLE photos (
-  photo_id int NOT NULL,
+  photo_id int NOT NULL PRIMARY KEY,
   photo_url varchar(255),
 	answer_id int,
-  PRIMARY KEY (photo_id),
   FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
 );
 
